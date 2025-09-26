cdf7b172cfaa162ee305bac2cc861ca995d52fc4
anuradhedilshan <anuradhedilshan@gmail.com>
2025-09-26 08:09:44 +0530

    fix buffer line and tmux

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     require('bufferline').setup {
       options = {
+        numbers = 'ordinal',
         diagnostics = 'nvim_lsp',
         show_buffer_close_icons = true,
         show_close_icon = true,
